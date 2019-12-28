753bfcf87296ad2d945b670d81c5a8957f0afa89
Amit Mukherjee <akmukherjee@gmail.com>
2019-12-27 21:32:43 -0500

    Adding the Projection Params to index.js and config file

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ async function getMatchData(){
 	//console.log(secrets);
 	//Use the Secrets to get the Okta Auth Bearer Token
 	let token = await require('./modules/tokens.js').getOktaToken(secrets);
-	//console.log(token);
+	console.log('Token acquired!');
 
 	//Get the list of Arms from the config file
 	armIds = config.armIds;
@@ -27,7 +27,7 @@ async function getMatchData(){
 	//Getting a list of Patients
 	patientsList = await Promise.all(promisesList);
 	//console.log(patientsList[0].get(armIds[0]).length);
-	console.log('List of Patients received');
+	console.log('List of Patients received!');
 
 	const patientFiles = require('./modules/patient-individual.js');
 	//This is to store the list of promises to get the S3 URLs per Arm
@@ -43,15 +43,15 @@ async function getMatchData(){
 		promisesFileList=[];
 		for(j=0;j<patientsList[id].get(armIds[id]).length;j++){
 			//patiendId= patientsList[id].get(armIds[id])[j];
-			promisesFileList.push(patientFiles.getPatientPromise(patientsList[id].get(armIds[id])[j],token));
+			promisesFileList.push(patientFiles.getPatientPromise(patientsList[id].get(armIds[id])[j],token,config.fileProjectionQuery));
 
 		}
 		FileList[id] = await Promise.all(promisesFileList);	
 	}
 
 	//FileList = await Promise.all(promisesFileList);
-	console.log(FileList);
-	console.log('List of Files received');
+	console.log(FileList[0]);
+	console.log('List of Files received!');
 
 
 
