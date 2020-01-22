5d681c13f65cf4cd5db85b42b3aaf9ee550f5cd3
Amit Mukherjee <akmukherjee@gmail.com>
2020-01-22 11:05:34 -0500

    Added Code to Upload Test Files. Also Wrapped Code in a try except block

--- matchMain.py
+++ matchMain.py
@@ -6,81 +6,82 @@ from datetime import datetime
 import json
 import jsonpickle
 import sys
-# Read the Configuration File
-with open('./config/config.json') as config_file:
-    data = json.load(config_file)
 
-# Read the region
-region = data['region']
-# Get List of Arms
-armIds = data['armIds']
-acls = data['phsIds']
-bucketNames = data["bucketNames"]
-domain = data["domain"]
-if((data['useProd']) == 'TRUE'):
-    print('Using Match Production Environment')
-    # Get the Secret Name
-    secret_name = data['secretNameProd']
-    # Get Okta Authorization URL
-    oktaAuthUrl = data["oktaAuthUrlProd"]
-    # Get the Match Treatment Arm Api URL
-    matchBaseUrl = data['matchProdBaseUrl']
-    # Get the Match Patient Api URL
-    matchBaseUrlPatient = data['matchProdBaseUrlPatient']
-else:
-    print('Using Match UAT Environment')
-    # Get the Secret Name UAT
-    secret_name = data['secretName']
-    # Get Okta UAT Authorization URL
-    oktaAuthUrl = data["oktaAuthUrl"]
-    # Get the Match UAT Treatment Arm Api URL
-    matchBaseUrl = data['matchUatBaseUrl']
-    # Get the Match UAT Patient Api URL
-    matchBaseUrlPatient = data['matchUatBaseUrlPatient']
+try:
+        # Read the Configuration File
+    with open('./config/config.json') as config_file:
+        data = json.load(config_file)
+
+    # Read the region
+    region = data['region']
+    # Get List of Arms
+    armIds = data['armIds']
+    acls = data['phsIds']
+    bucketNames = data["bucketNames"]
+    domain = data["domain"]
+    useProd = data["useProd"]
+    if((data['useProd']) == 'TRUE'):
+        print('Using Match Production Environment')
+        # Get the Secret Name
+        secret_name = data['secretNameProd']
+        # Get Okta Authorization URL
+        oktaAuthUrl = data["oktaAuthUrlProd"]
+        # Get the Match Treatment Arm Api URL
+        matchBaseUrl = data['matchProdBaseUrl']
+        # Get the Match Patient Api URL
+        matchBaseUrlPatient = data['matchProdBaseUrlPatient']
+    else:
+        print('Using Match UAT Environment')
+        # Get the Secret Name UAT
+        secret_name = data['secretName']
+        # Get Okta UAT Authorization URL
+        oktaAuthUrl = data["oktaAuthUrl"]
+        # Get the Match UAT Treatment Arm Api URL
+        matchBaseUrl = data['matchUatBaseUrl']
+        # Get the Match UAT Patient Api URL
+        matchBaseUrlPatient = data['matchUatBaseUrlPatient']
+
+    # Get Projection Query to read List of Files
+    fileProjectionQuery = data['fileProjectionQuery']
+
+    # Read Secrets from AWS Secrets Manager
+    secrets = get_secret(region, secret_name)
+    print('Secrets Read')
+    # Retrieve the Okta Token
+    token = get_okta_token(secrets, data, oktaAuthUrl)
+    print('Token Obtained')
+
+    myPatientList = []
+    # Get the List of Patients for Each Arm
+    getPatientsByTreatmentArm(
+        armIds, token, matchBaseUrl, myPatientList, acls, bucketNames)
+    print('List of Patients by Arm received')
+
+    # Get the List of S3 Paths for each patient in each Arm
+    getPatientsFileData(token, fileProjectionQuery,
+                        matchBaseUrlPatient, myPatientList)
+    print('Getting S3 Paths for all Patients in each Arm')
+
+    print('List of File Paths received')
+    getPatientsPreSignedURL(token, matchBaseUrlPatient, myPatientList)
+
+    print('PreSigned Urls Generated')
+
+    # Getting Bucket Name to upload files
+    bucketName = secrets["S3_DEST_BUCKET_NAME"]
+
+    print('Uploading Files...')
+
+    manifest_filename = 'Manifest' + \
+        str(datetime.now().strftime('%Y_%m_%d_%H_%M_%S')) + '.csv'
+
+    uploadPatientFiles(manifest_filename, myPatientList, domain, useProd)
+    print('Uploading Files Completed!')
+    # Exiting Code
+    # print(jsonpickle.encode(myPatientList))
+    # sys.exit(0)
 
 
-# Get Projection Query to read List of Files
-fileProjectionQuery = data['fileProjectionQuery']
-
-# Read Secrets from AWS Secrets Manager
-secrets = get_secret(region, secret_name)
-print('Secrets Read')
-# Retrieve the Okta Token
-token = get_okta_token(secrets, data, oktaAuthUrl)
-print('Token Obtained')
-
-myPatientList = []
-# Get the List of Patients for Each Arm
-getPatientsByTreatmentArm(
-    armIds, token, matchBaseUrl, myPatientList, acls, bucketNames)
-print('List of Patients by Arm received')
-
-# Printing List of Patients for Testing
-# print(jsonpickle.encode(myPatientList))
-
-fileListPatientArm = []
-i = 0
-# Get the List of S3 Paths for each patient in each Arm
-getPatientsFileData(token, fileProjectionQuery,
-                    matchBaseUrlPatient, myPatientList)
-print('Getting S3 Paths for all Patients in each Arm')
-
-print('List of File Paths received')
-getPatientsPreSignedURL(token, matchBaseUrlPatient, myPatientList)
-
-print('PreSigned Urls Generated')
-
-# Getting Bucket Name to upload files
-bucketName = secrets["S3_DEST_BUCKET_NAME"]
-
-print('Uploading Files...')
-
-manifest_filename = 'Manifest' + \
-    str(datetime.now().strftime('%Y_%m_%d_%H_%M_%S')) + '.csv'
-
-uploadPatientFiles(manifest_filename, myPatientList, domain)
-# Exiting Code
-print(jsonpickle.encode(myPatientList))
-sys.exit(0)
-i, j = 0, 0
-print('Uploading Files Completed!')
+except Exception as e:
+    # Print the cause of the exception
+    print(e)
--- patient.py
+++ patient.py
@@ -7,15 +7,19 @@ import jsonpickle
 import json
 import random
 import string
+import sys
 from bento.common.utils import get_md5, UUID, get_sha512, get_uuid
 
 # Specifying the Constants for the Manifest File
 GUID = 'GUID'
+UUID = 'uuid'
+TYPE = "type"
 ACL = 'acl'
 MD5 = 'md5'
 SIZE = 'size'
 URL = 'url'
 FILE_NAME = 'file_name'
+FILE_NODE = "file"
 DEFAULT_ACL = "['Open']"
 FILE_LOC = 'file_locations'
 FILE_FORMAT = 'file_format'
@@ -25,10 +29,11 @@ FILE_TYPE = "file_type"
 MD5SUM = "md5sum"
 CASE_ID = "case_id"
 FILE_STATUS = "file_status"
+MSN = "molecularSequenceNumber"
 
 INDEXD_GUID_PREFIX = 'dg.4DFC/'
-MANIFEST_FIELDS = [GUID, MD5, MD5SUM, SIZE,
-                   ACL, URL, FILE_LOC, FILE_NAME, FILE_STATUS, FILE_FORMAT, FILE_TYPE, CASE_ID]
+MANIFEST_FIELDS = [GUID, UUID, MD5, MD5SUM, TYPE, SIZE,
+                   ACL, URL, FILE_LOC, FILE_NAME, FILE_STATUS, FILE_FORMAT, FILE_TYPE, MSN]
 
 # Subpath for PreSigned URL request
 DOWNLOAD_URL_SUBPATH = '/download_url'
@@ -103,50 +108,59 @@ def getPatientS3Paths(data, patientFromList=None):
                 fileInfo = {}
                 if('dnaBamFilePath' in nextGenerationSequence['ionReporterResults']):
                     fileInfo["type"] = 'DNABam'
+                    fileInfo["molecularSequenceNumber"] = nextGenerationSequence['ionReporterResults']["molecularSequenceNumber"]
                     fileInfo["s3url"] = nextGenerationSequence['ionReporterResults']['dnaBamFilePath']
                     patientFromList.files.append(fileInfo)
 
                 fileInfo = {}
                 if('rnaBamFilePath' in nextGenerationSequence['ionReporterResults']):
                     fileInfo["type"] = 'RNABam'
+                    fileInfo["molecularSequenceNumber"] = nextGenerationSequence['ionReporterResults']["molecularSequenceNumber"]
                     fileInfo["s3url"] = nextGenerationSequence['ionReporterResults']['rnaBamFilePath']
                     patientFromList.files.append(fileInfo)
 
                 fileInfo = {}
                 if('vcfFilePath' in nextGenerationSequence['ionReporterResults']):
                     fileInfo["type"] = 'VCF'
+                    fileInfo["molecularSequenceNumber"] = nextGenerationSequence['ionReporterResults']["molecularSequenceNumber"]
                     fileInfo["s3url"] = nextGenerationSequence['ionReporterResults']['vcfFilePath']
                     patientFromList.files.append(fileInfo)
 
                 fileInfo = {}
                 if('dnaBaiFilePath' in nextGenerationSequence['ionReporterResults']):
                     fileInfo["type"] = 'DNABai'
+                    fileInfo["molecularSequenceNumber"] = nextGenerationSequence['ionReporterResults']["molecularSequenceNumber"]
                     fileInfo["s3url"] = nextGenerationSequence['ionReporterResults']['dnaBaiFilePath']
                     patientFromList.files.append(fileInfo)
 
                 fileInfo = {}
                 if('rnaBaiFilePath' in nextGenerationSequence['ionReporterResults']):
                     fileInfo["type"] = 'RNABai'
+                    fileInfo["molecularSequenceNumber"] = nextGenerationSequence['ionReporterResults']["molecularSequenceNumber"]
                     fileInfo["s3url"] = nextGenerationSequence['ionReporterResults']['rnaBaiFilePath']
                     patientFromList.files.append(fileInfo)
 
 
-def uploadPatientFiles(manifestpath='', myPatientList=[], domain=''):
+def uploadPatientFiles(manifestpath='', myPatientList=[], domain='', useProd='False'):
     """
     This function uploads a set of files file pointed to from the Presigned 
     urls into a bucket with the specified key name.The manifestpath is where the file final manifest is stored.
     """
-    session = boto3.Session()
-    s3 = session.resource('s3')
+
+    s3_client = boto3.client('s3')
 
     # Open the IndexD Manifest File
     with open(manifestpath, 'w', newline='\n') as indexd_f:
 
         manifest_writer = csv.DictWriter(
             indexd_f, delimiter='\t', fieldnames=MANIFEST_FIELDS)
+
+        # Write the Header Row
         manifest_writer.writeheader()
+        # Process each Patient in the list
         for patient in myPatientList:
-            bucket = s3.Bucket(patient.bucket)
+            # Get the name of the bucket
+            bucket = patient.bucket
             for fileData in (patient.files):
                 # Get the File using the PreSigned URL
                 url = fileData['download_url']
@@ -156,20 +170,52 @@ def uploadPatientFiles(manifestpath='', myPatientList=[], domain=''):
 
                 # Get the Filename from the PreSigned URL
                 filename = url.split("?")[0].split('/')[::-1][0]
-                filenameToUpload = 'sample.bam'
-                s3_key = patient.patientId+'/'+filename
-                # Write the file to local disk
-                # with open(filename, 'wb') as file:
-                #     if(r.content is not None):
-                #         file.write(r.content)
+
+                # Creating a pseudo variable for Non Production Test Data for different File types
+                if(useProd == 'False'):
+                    if(fileData["type"] == 'DNABam'):
+                        filenameToUpload = './samples/SampleFile.bam'
+                    if(fileData["type"] == 'RNABam'):
+                        filenameToUpload = './samples/Sample2.bam'
+                    if(fileData["type"] == 'VCF'):
+                        filenameToUpload = './samples/Sample.vcf'
+                    if(fileData["type"] == 'DNABai'):
+                        filenameToUpload = './samples/SampleFile.bam.bai'
+                    if(fileData["type"] == 'RNABai'):
+                        filenameToUpload = './samples/Sample2.bam.bai'
+                else:
+                    # This is Production. Write the file to local disk
+                    with open(filename, 'wb') as file:
+                        if(r.content is not None):
+                            file.write(r.content)
+                        # Setting the Pseudo variable's name to the actual filename
+                        filenameToUpload = filename
+
+                # Set S3 Key to Match the MATCH MSN/FileName
+                msn = fileData["molecularSequenceNumber"]
+                s3_key = msn+'/'+filename
 
                 # Upload File to S3
-                bucket.upload_file(Filename=filenameToUpload, Key=s3_key)
+                #bucket.upload_file(Filename=filenameToUpload, Key=s3_key)
+                with open(filenameToUpload, 'rb') as file:
+                    s3_client.put_object(Bucket=bucket, Key=s3_key, Body=file)
+
+                md5sum = s3_client.head_object(
+                    Bucket=bucket, Key=s3_key)['ETag'][1:-1]
 
                 # Calculate MD5 , Size and Bucket Location
                 md5 = get_md5(filenameToUpload)
+
+                if(md5sum != md5):
+                    print(f"Error Uploading file: {filename}")
+                    sys.exit(1)
+
+                print(f'Uploaded file {filenameToUpload}')
+                # Get S3 location of the bucket
                 s3_location = "s3://{}/{}".format(patient.bucket, s3_key)
+                # Get the filesize of the downloaded file from MATCHBOX
                 file_size = os.stat(filenameToUpload).st_size
+                # Get the File format of the file downloaded
                 file_format = (os.path.splitext(filenameToUpload)
                                [1]).split('.')[1].lower()
                 # Calculating the SHA512
@@ -177,14 +223,18 @@ def uploadPatientFiles(manifestpath='', myPatientList=[], domain=''):
                 sha512 = get_sha512(currentDirectory+'\\'+filenameToUpload)
                 # Delete the file once it has been processed
                 print(f'Processed File {filename} ')
-
+                # Extract the ACL from the acls provided
                 acl = "[{}]".format(patient.acls)
-                guid = '{}{}'.format(INDEXD_GUID_PREFIX,
-                                     get_uuid(domain, "file", sha512))
+                # Generate the UUID and GUID using the SHA calculated
+                uuid = get_uuid(domain, "file", sha512)
+                guid = '{}{}'.format(INDEXD_GUID_PREFIX, uuid)
+                # Generate the dictionary to be writtent to the manifest file
                 fileInfo = {
                     GUID: guid,
+                    UUID: uuid,
                     MD5: md5,
                     MD5SUM: md5,
+                    TYPE: FILE_NODE,
                     SIZE: file_size,
                     ACL: acl,
                     URL: s3_location,
@@ -193,22 +243,13 @@ def uploadPatientFiles(manifestpath='', myPatientList=[], domain=''):
                     FILE_STATUS: "uploaded",
                     FILE_FORMAT: file_format,
                     FILE_TYPE: fileData["type"],
-                    CASE_ID: patient.patientId
+                    MSN: msn
 
                 }
                 print(jsonpickle.encode(fileInfo))
+                # Write the entry into the Manifest file
                 manifest_writer.writerow(fileInfo)
-
-                # if os.path.exists(filename):
-                #     os.remove(filename)
-        # Write all the files for this patient
-
-    #     # Write the Header if this is the first time entering the file
-    #     if indexd_f.tell() == 0:
-    #         manifest_writer.writeheader()
-
-    #     # Generate the GUID
-    #     # After Upload, Generate the Path Location
-    #     # Write all these values to the Manifest File
-
-        #
+                # For Production Data Delete the File after Processing
+                if(useProd == True):
+                    if os.path.exists(filename):
+                        os.remove(filename)
