fcb0c4debecc577b153e5da78df44d0478b6465c
Fuyuan Wang <wfy199719971997@gmail.com>
2022-09-07 16:17:13 -0400

    If a clinical_trial.clinical_trial_id column exists in raw file, then copy that column, otherwise add a hard coded column

--- transformation_script/arm.py
+++ transformation_script/arm.py
@@ -8,13 +8,16 @@ def arm_transformation(arm_file_name, log):
     #log = get_logger('Arm Transformation')
     log.info('Transforming arm.csv')
     arm_df = pd.read_csv(arm_file_name)
+    arm_columns = list(arm_df.columns)
     new_arm_id = []
     for arm_id in arm_df['arm_id']:
         arm_id_list = arm_id.split('-')
         new_arm_id.append(arm_id_list[1])
     arm_df['arm_id'] = new_arm_id
+    clinical_trial_id = 'clinical_trial.clinical_trial_id'
+    if not clinical_trial_id in arm_columns:
+        arm_df['clinical_trial.clinical_trial_id'] = ['NCT02465060'] * len(arm_df)
     props = [
-        {'new_property':'clinical_trial.clinical_trial_id', 'new_value':['NCT02465060'] * len(arm_df)},
         {'new_property':'type', 'new_value':['arm'] * len(arm_df)},
         {'new_property':'show_node', 'new_value':['TRUE'] * len(arm_df)}
     ]
